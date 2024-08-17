594a6a4d2003e1c709dcb2eda29206c75d937a40
Kevin Wild <deftbuncase@gmail.com>
2024-08-18 01:21:23 +0300

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,25 @@
-@app.route('/' + TOKEN, methods=['POST'])
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, CallbackContext
+
+app = Flask(__name__)
+
+TOKEN = '7362610114:AAHRCtTThfAs0Q-mZjAVqlSotwiLhxlRIzs'
+bot = Bot(token=TOKEN)
+
+dispatcher = Dispatcher(bot, None, use_context=True)
+
+# Команды и обработчики
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Привет! Подпишитесь на канал и получите доступ к чату.')
+
+dispatcher.add_handler(CommandHandler('start', start))
+
+@app.route(f'/{TOKEN}', methods=['POST'])
 def respond():
     update = Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
     return 'ok'
+
+if __name__ == '__main__':
+    app.run(debug=True)
