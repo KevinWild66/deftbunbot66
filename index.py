9fa767887f027acd74974367cb36684079bd9a8d
Kevin Wild <deftbuncase@gmail.com>
2024-08-17 23:01:35 +0300

    Initial commit

--- index.py
+++ index.py
@@ -7,11 +7,11 @@ CHANNEL_USERNAME = "@deftbuncrypto"
 PRIVATE_CHAT_LINK = "https://t.me/+QHPRPFA4sPlmNjYy"
 
 app = Flask(__name__)
+bot = Bot(token=TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
     bot = context.bot
-
     member_status = await bot.get_chat_member(chat_id=CHANNEL_USERNAME, user_id=user.id)
 
     if member_status.status in ['member', 'administrator', 'creator']:
@@ -22,16 +22,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler('start', start))
-
     await application.initialize()
     await application.start()
     await application.updater.start_polling()
 
-@app.route(f"/{TOKEN}", methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    update = Update.de_json(request.get_json(force=True), Bot(TOKEN))
-    asyncio.run(main())
-    return "OK"
+    update = Update.de_json(request.get_json(force=True), bot)
+    application = Application.builder().token(TOKEN).build()
+    application.process_update(update)
+    return 'OK'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
