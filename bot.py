765145655803fe69ca3f9f01db46e852017bfc3b
Kevin Wild <deftbuncase@gmail.com>
2024-08-17 17:14:56 +0300

    Описание изменений

--- bot.py
+++ bot.py
@@ -1,33 +1,38 @@
-from telegram import Update, Bot
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 TOKEN = "7362610114:AAHRCtTThfAs0Q-mZjAVqlSotwiLhxlRIzs"
 CHANNEL_USERNAME = "@deftbuncrypto"
 PRIVATE_CHAT_LINK = "https://t.me/+QHPRPFA4sPlmNjYy"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.message.from_user
+    keyboard = [
+        [InlineKeyboardButton("Подтвердить подписку", callback_data='check_subscription')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Пожалуйста, подпишитесь на https://t.me/deftbuncrypto, чтобы получить доступ к чату.", reply_markup=reply_markup)
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    
+    user = query.from_user
     bot = context.bot
-
     member_status = await bot.get_chat_member(chat_id=CHANNEL_USERNAME, user_id=user.id)
-
+    
     if member_status.status in ['member', 'administrator', 'creator']:
-        await update.message.reply_text(f"Спасибо за подписку! Вот ссылка на чат: {PRIVATE_CHAT_LINK}")
+        await query.edit_message_text(text=f"Спасибо за подписку! Вот ссылка на чат: {PRIVATE_CHAT_LINK}")
     else:
-        await update.message.reply_text("Пожалуйста, подпишитесь на канал, чтобы получить доступ к чату.")
+        await query.edit_message_text(text="Пожалуйста, подпишитесь на канал, чтобы получить доступ к чату.")
 
-async def main():
+def main():
     application = Application.builder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
 
-    # Запуск бота
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.updater.stop()
-    await application.stop()
+    application.run_polling()
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+if __name__ == '__main__':
+    main()
+
