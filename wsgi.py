55b03a0c74e2b650672d5010a89f434b597eb674
Kevin Wild <deftbuncase@gmail.com>
2024-08-18 01:38:00 +0300

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -9,18 +9,17 @@ bot = Bot(token=TOKEN)
 
 dispatcher = Dispatcher(bot, None, use_context=True)
 
-# Команды и обработчики
+# Обработчик команды /start
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('Привет! Подпишитесь на канал и получите доступ к чату.')
 
 dispatcher.add_handler(CommandHandler('start', start))
 
-@app.route('/7362610114:AAHRCtTThfAs0Q-mZjAVqlSotwiLhxlRIzs', methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def respond():
     update = Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
     return 'ok'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
